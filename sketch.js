fb54b6cacad8cfac3eb1070eaba11f8fbe3a2c6e
SaveTheDream5s <djedsall1@gmail.com>
2026-02-21 00:59:42 -0500

    Update random position generation for canvas size

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ function setup() {
 createCanvas(windowWidth, windowHeight);
 for (let i = 0; i < 100; i++) {
 population.push({
-x: random(width),
-y: random(height),
+x: random(windowWidth),
+y: random(windowHeight),
 vx: random(-2, 2),
 vy: random(-2, 2)
 });
@@ -22,5 +22,11 @@ circle(p.x, p.y, 10);
 p.x += p.vx;
 p.y += p.vy;
 
+if (p.x < 0 || p.x > width) {
+p.vx = p.vx * -1;
+}
+if (p.y < 0 || p.y > height) {
+p.vy = p.vy * -1;
+}
 }
 }
